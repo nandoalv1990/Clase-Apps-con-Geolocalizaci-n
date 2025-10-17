5a80440117141198b6d58d6e0fedb129ca04f46e
Fernando Alvarez <nandoalv1990@gmail.com>
2025-10-17 15:20:59 -0600

    Primer commit

---
+++ proyectoflutter1/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowMaterialGrid: false,
+      home: PantallaPrincipal(),
+    );
+  }
+}
+
+class PantallaPrincipal extends StatefulWidget {
+  const PantallaPrincipal({super.key});
+
+  @override
+  State<PantallaPrincipal> createState() => _PantallaPrincipalState();
+}
+
+class _PantallaPrincipalState extends State<PantallaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
