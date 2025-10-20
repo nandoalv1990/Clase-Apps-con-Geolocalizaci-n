b447bca6ca983ec5d0f6bed2271fd6c25db83a55
Fernando Alvarez <nandoalv1990@gmail.com>
2025-10-20 11:32:02 -0600

    App finalizada

--- proyectoflutter2/lib/main.dart
+++ proyectoflutter2/lib/main.dart
@@ -6,6 +6,8 @@ void main() {
 
 
 class CalculadoraSumaApp extends StatelessWidget {
+  const CalculadoraSumaApp({super.key});
+
   @override 
 
   Widget build(BuildContext context) {
@@ -18,6 +20,8 @@ class CalculadoraSumaApp extends StatelessWidget {
 }
 
 class CalculadoraUI extends StatefulWidget {
+  const CalculadoraUI({super.key});
+
   @override
   _CalculadoraUIState createState() => _CalculadoraUIState();
 }
@@ -35,8 +39,68 @@ class _CalculadoraUIState extends State<CalculadoraUI> {
     });
   }
   @override 
-  widget @override
   Widget build(BuildContext context) {
-    return ;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Suma con layouts'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Padding(padding: const EdgeInsets.all(20.0),
+      child: Column(
+        // Layout principal: organización vertical
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Calculadora básica',
+            style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 30),
+          Row(
+            // Layout secundario: organiza horizontalmente los TextFields
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Expanded(
+                child: TextField(
+                  controller: num1Controller,
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                    labelText: 'Número 1',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(width: 10,),
+              Expanded(child: TextField(
+                controller: num2Controller,
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Número 2',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20,),
+          ElevatedButton(
+          onPressed: sumar, 
+          child: Text('sumar'),
+          ),
+          SizedBox(height: 10,),
+          Container(
+            padding: EdgeInsets.all(15),
+            decoration: BoxDecoration(
+              color: Colors.blue[50],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(
+              'Resultado: $resultado',
+              style: TextStyle(fontSize: 20.0),
+            ),
+          )
+        ],
+      ),
+      ),
+    );
   }
 }
