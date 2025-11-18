6ce7b296ced51d7b7b22e83ee286a47b6085c7cc
Fernando Alvarez <nandoalv1990@gmail.com>
2025-11-18 11:29:07 -0600

    maps paso uno

--- proyectoflutter7/lib/src/splash_page.dart
+++ proyectoflutter7/lib/src/splash_page.dart
@@ -1,4 +1,6 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
@@ -8,8 +10,50 @@ class SplashPage extends StatefulWidget {
 }
 
 class _SplashPageState extends State<SplashPage> {
+GoogleMapController? _mapController;
+//20.527102, -100.8116
+LatLng _currentPosition = const LatLng(20.527102, -100.8116);
+final Set<Marker> _markers = {};
+
   @override
+  void initState(){
+    super.initState();
+    _determinePosition();
+  }
+
+  // Obtener ubicación actual del usuario
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Comprobar que el GPS está activo
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled){
+      return;
+    }
+
+  // Permisos
+  permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied){
+    permission == await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied){
+      return;
+    }
+  }
+  if (permission == LocationPermission.deniedForever){
+    return;
+  }
+  // Obtener posición
+  Position position = await Geolocator.getCurrentPosition();
+
+  setState(() {
+    _currentPosition = LatLng(position.latitude, position.longitude);
+  });
+}
+
+
   Widget build(BuildContext context) {
     return Container();
   }
+  
 }
